f4f8a91c7b206d9631436d0a636f6138fc1ab631
181 <yuiwai.dev@gmail.com>
2012-12-28 11:44:38 +0900

    Add icons in the menu
    v3

--- iplbase/wiki.js
+++ iplbase/wiki.js
@@ -1,3 +1,3 @@
 $(function(){
-  $("body").prepend('<ul class="iplheader"><li><a href="http://www.iplwiki.info">Wiki</a></li><li><a href="http://as.iplwiki.info">Chat</a></li><li><a href="http://qa.iplwiki.info/">Q&amp;A</a></li><li><a href="https://redmine.iplwiki.info">Redmine</a></li><li><a href="http://tg.iplwiki.info/tuigwaa/appstore/">AppStore</a></li></ul>');
+  $("body").prepend('<ul class="iplheader"><li><a href="http://www.iplwiki.info"><i class="icon-globe"></i>Wiki</a></li><li><a href="http://as.iplwiki.info"><i class="icon-comment"></i>Chat</a></li><li><a href="http://qa.iplwiki.info/"><i class="icon-question-sign"></i>Q&amp;A</a></li><li><a href="https://redmine.iplwiki.info"><i class="icon-road"></i>Redmine</a></li><li><a href="http://tg.iplwiki.info/tuigwaa/appstore/"><i class="icon-shopping-cart"></i>AppStore</a></li></ul>');
 });
