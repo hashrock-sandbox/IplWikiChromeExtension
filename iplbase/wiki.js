0bb5d6e99607aaf380ca2c38aa8086267fcb8dd2
181 <yuiwai.dev@gmail.com>
2012-12-28 10:44:22 +0900

    initial commit

---
+++ iplbase/wiki.js
@@ -0,0 +1,3 @@
+$(function(){
+  $("body").prepend('<ul class="iplheader"><li><a href="http://www.iplwiki.info">Wiki</a></li><li><a href="http://as.iplwiki.info">Chat</a></li><li><a href="http://qa.iplwiki.info/">Q&amp;A</a></li><li><a href="https://redmine.iplwiki.info">Redmine</a></li><li><a href="http://tg.iplwiki.info/tuigwaa/appstore/">AppStore</a></li></ul>');
+});
